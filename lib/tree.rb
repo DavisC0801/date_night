2d59333ab660103b640bbe1d9c1bc1965c4fd784
davisc0801 <davisc1014@gmail.com>
2019-07-15 10:08:03 -0600

    Add insert method to tree class

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,8 +1,34 @@
 require "./lib/node"
 
 class Tree
+  attr_reader :root
 
   def initialize
     @root = nil
+  end
+
+  def insert(score, title)
+    return nil if title.empty? || score.nil?
+    current_node = @root
+    depth = 0
+    loop do
+      if current_node.nil?
+        current_node = Node.new(score, title)
+        @root = current_node if @root.nil?
+        return depth
+      elsif score > current_node.score && current_node.right_node.nil?
+        current_node.right_node = Node.new(score, title)
+        return depth + 1
+      elsif score < current_node.score && current_node.left_node.nil?
+        current_node.left_node = Node.new(score, title)
+        return depth + 1
+      elsif score > current_node.score
+        current_node = current_node.right_node
+        depth += 1
+      elsif score < current_node.score
+        current_node = current_node.left_node
+        depth += 1
+      end
+    end
   end
 end
