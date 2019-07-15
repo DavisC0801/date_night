7361909089ad6f7e70ea3b46fd4bd5f8215d0824
davisc0801 <davisc1014@gmail.com>
2019-07-15 09:18:16 -0600

    Add tests for additional node functionality

--- test/node_test.rb
+++ test/node_test.rb
@@ -12,4 +12,20 @@ class NodeTest < Minitest::Test
   def test_it_exists
     assert_instance_of Node, @n
   end
+
+  def test_it_has_attributes
+    assert @n.score, 50
+    assert @n.title, "test"
+  end
+
+  def test_it_has_nil_nodes_by_default
+    assert_nil @n.left_node
+    assert_nil @n.right_node
+  end
+
+  def test_it_can_determine_if_leaf
+    assert @n.leaf?
+    @n.left_node = Node.new(40, "test2")
+    refute @n.leaf?
+  end
 end
