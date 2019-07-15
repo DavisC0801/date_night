123e63a4d9dd290f24b7283de94a7b1312b808cf
davisc0801 <davisc1014@gmail.com>
2019-07-15 09:13:23 -0600

    Add basic node class

---
+++ lib/node.rb
@@ -0,0 +1,13 @@
+class Node
+
+  #add parent reader and arg in add menthod for double linked list
+  attr_accessor :left_node, :right_node
+  attr_reader :score, :title
+
+  def initialize(score, title)
+    @title = title
+    @score = score
+    @left_node = nil
+    @right_node = nil
+  end
+end
