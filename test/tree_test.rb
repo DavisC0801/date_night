458a797a918d5d6871a36d620ccf3823d7f92141
davisc0801 <davisc1014@gmail.com>
2019-07-15 10:07:47 -0600

    Add test for inserting nodes

--- test/tree_test.rb
+++ test/tree_test.rb
@@ -6,10 +6,25 @@ require "./lib/tree"
 class TreeTest < Minitest::Test
 
   def setup
-    @t = Tree.new
+    @tree = Tree.new
   end
 
   def test_it_exists
-    assert_instance_of Tree, @t
+    assert_instance_of Tree, @tree
+  end
+
+  def test_it_can_insert_nodes
+    depth_1 = @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    depth_2 = @tree.insert(16, "Johnny English")
+    depth_3 = @tree.insert(92, "Sharknado 3")
+    depth_4 = @tree.insert(50, "Hannibal Buress: Animal Furnace")
+    assert_equal @tree.root.score, 61
+    assert_equal @tree.root.left_node.score, 16
+    assert_equal @tree.root.right_node.score, 92
+    assert_equal @tree.root.left_node.right_node.score, 50
+    assert_equal depth_1, 0
+    assert_equal depth_2, 1
+    assert_equal depth_3, 1
+    assert_equal depth_4, 2
   end
 end
